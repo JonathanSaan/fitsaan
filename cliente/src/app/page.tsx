7a48f24595313c0f7940a33c1b7fc915235f427e
jonathansaandev <jonathansaandev@gmail.com>
2024-03-17 20:35:30 -0300

    Add Link component and update page layout

--- cliente/src/app/page.tsx
+++ cliente/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Image from "next/image";
 import Header from "../components/Header";
 import bannerDesktop from "../../public/man_doing_front_lever-desktop.webp";
@@ -7,20 +8,49 @@ const Home = () => {
   return (
     <>
       <Header />
-      <main className="flex flex-col min-h-screen bg-black pt-20">
-        <div className="max-md:hidden">
-          <Image src={bannerDesktop} alt="a man exercising dumbbell" placeholder="blur" priority className="object-cover w-screen pointer-events-none" />
+      <main className="flex flex-col min-h-screen bg-black">
+        <div className="relative">
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-black"></div>
+          <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black"></div>
+          <section className="absolute max-md:top-1/2 max-md:left-1/2 max-md:transform max-md:-translate-x-1/2 max-md:-translate-y-1/2 text-center text-white md:mt-[11vw] md:ml-[3vw] max-sm:w-full md:min-w-[53rem] xl:min-w-[73rem] sm:w-[90vw] md:w-[58vw] xl:w-[50rem]">
+            <h1 className="font-semibold max-sm:text-2xl max-md:text-4xl max-xl:text-5xl text-7xl">
+              Your Fitness and Wellness Community
+            </h1>
+            <p className="max-md:mt-4 max-md:mb-10 mt-6 mb-12 px-[2rem] sm:text-xl xl:text-2xl">
+              Join our fitness and wellness community! At Fitsaan, you'll
+              discover a collaborative environment to train, connect with other
+              enthusiasts, and participate in challenges to achieve your health
+              and happiness goals.
+            </p>
+            <Link
+              href="/sign-up"
+              className="bg-red hover:bg-red/85 py-4 px-8 uppercase rounded-xl"
+            >
+              Join Now!
+            </Link>
+          </section>
+          <Image
+            src={bannerDesktop}
+            alt="a man exercising dumbbell"
+            placeholder="blur"
+            priority
+            className="max-md:hidden object-cover w-screen pointer-events-none"
+          />
+          <Image
+            src={bannerMobile}
+            alt="a man exercising dumbbell"
+            placeholder="blur"
+            priority
+            className="md:hidden object-cover h-[100vw] w-screen pointer-events-none"
+          />
         </div>
-        <div className="md:hidden">
-          <Image src={bannerMobile} alt="a man exercising dumbbell" placeholder="blur" priority className="object-cover h-[100vw] w-screen pointer-events-none" />
-        </div>
-          
+
         <div className="py-7 px-10 lg:py-11 lg:px-20">
           <h2 className="text-white text-3xl text-center">Home</h2>
         </div>
       </main>
     </>
   );
-}
+};
 
 export default Home;
